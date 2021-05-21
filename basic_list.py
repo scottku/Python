dc1729e6bb3ae25227812d09c1a032ab038a0949
구본재 <scottku@naver.com>
2021-05-21 11:34:31 +0900

    list 메서드, stack, queue

--- basic_list.py
+++ basic_list.py
@@ -96,8 +96,87 @@ def list_oper():
     print("SUM: " , sum(lst))
     print("MIN: ", min(lst))
     print("MAX: ", max(lst))
-    print("Average: ", sum(lst)/len(lst))
+    print("Average: ", sum(lst)/len(lst)) # AVG 메서드는 없음
+
+def list_method():
+    """ 리스트 메서드들 """
+    lst = [10, 2, 22, 9, 8, 33, 4, 12]
+    print("원본: ", lst)
+    copy = lst.copy()   # 복제 메서드
+    # Reverse : 리스트의 반전
+    copy.reverse()
+    print("Reverse: ", copy)
+
+    copy = lst.copy()
+    print("원본: ", copy)
+
+    # 정렬: sort
+    # 메서드로서의 sort -> 내부 데이터를 실제 sort
+    # 문법으로서의 sorted -> 정렬된 새 리스트를 반환
+
+    result = sorted(copy) # copy를 정렬 -> 새 리스트로 반환
+    print("Sorted ASC: ", result)   # 오름차순
+    result = sorted(copy,reverse=True) # 내림차순 정렬
+    print("Sorted DESC: ", result)
+    # 정렬 키 함수 정의
+    # 정렬 키 함수를 전달 -> 정렬 기준을 변경
+    print("원본: ", copy)
+    result = sorted(copy, key=str) # 키 함수를 str로 변경
+    print("Sorted key=str: ", result) # 문자열은 앞글자가 가장 빠른것부터 정렬 -> 1~9
+    # 정렬 기준의 사용자 정의
+    # 리스트의 요소를 5로 나눈 나머지의 역순으로 정렬
+    def key_func(val): # 사용자 정의 키 함수
+        return val % 5
+    result = sorted(copy, key=key_func, reverse=True)
+
+    print("Sorted key=custom, DESC: ", result)
+
+    # sorted 함수 -> 원본을 변경시키지 않음
+    # sort 메서드 -> 원본 내부 데이터를 변경
+    copy.sort(key=key_func, reverse=True)
+    print("Sort Method: ", copy)
+
+def stack_ex():
+    """ 리스트를 활용한 스택의 구현
+            append, pop 메서드
+    """
+    stack = []
+    stack.append[10]
+    stack.append[20]
+    stack.append[30]
+    print("Stack: ", stack)
+
+    # 인출
+    print("Pop ", stack.pop())
+    print("Pop: ", stack.pop())
+    print("Pop: ", stack.pop())
+    print("Pop: ", stack.pop())
+    if len(stack): # 스택이 비어있지 않으면
+        print("POP: ", stack.pop())
+    else:
+        print("스택이 비어있습니다")
+
+def qeueu_ex():
+    """ 리스트를 활용한 queue의 구현
+            append, pop(0)을 활용 구현
+            First Input First Output 자료형 """
+
+    queue = []
+    queue.append(10)
+    queue.append(20)
+    queue.append(30)
+
+    print("QERER: ", queue)
+
+    # 인출: pop(0)
+    print(queue.pop(0)) # 맨 앞에서부터 인출
+    print(queue.pop(0))
+    print(queue.pop(0))
+
 
 if __name__ == "__main__":
     # define_list()
-    list_oper()
+    # list_oper()
+    # list_method()
+    # stack_ex()
+    qeueu_ex()
