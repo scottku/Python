27c22c21f759cd885ca0395189e25ef08af30ab8
구본재 <scottku@naver.com>
2021-05-25 11:44:58 +0900

    피클 사용하기

---
+++ adv_fileio.py
@@ -0,0 +1,136 @@
+# 파일 모드
+#   action: r(읽기:default), w(쓰기), a(추가)
+#   type: t(텍스트:default), b(바이너리)
+import pickle
+
+
+def write01():
+    f = open("./sample/test.txt", "w", encoding="UTF-8") # write text
+    write_size = f.write("Life is too short, you need Python")
+    print(write_size)
+    f.close();
+
+
+def write02():
+    f = open("./sample/multilines.txt", "w", encoding="UTF-8")
+    for i in range(10):
+        f.write("Line %d\n" % i)
+    f.close()
+
+
+def read01():
+    f = open("./sample/multilines.txt", "r", encoding="UTF-8")
+    text = f.read()
+    print(text)
+    f.close()
+
+
+def read02():
+    # readline 메서드를 이용하면 한 줄 단위로 읽을 수 있다
+    f = open("./sample/multilines.txt", "r", encoding="UTF-8")
+    while True:
+        line = f.readline() # 한 줄 읽기
+        if not line:    # 더 읽을 내용이 없으면
+            break
+        print(line)
+    f.close()
+
+
+def read03():
+    # readlines 메서드 -> 전체 리스를 불러와서 리스트로 변환 제공
+    f = open("./sample/multilines.txt", "r", encoding="UTF-8")
+    lines = f.readlines()
+
+    for line in lines:
+        print(line)
+    f.close()
+
+def copy_binary():
+    # 바이너리 파일을 다루려면 모드를 b로 설정
+    # rose-flower.jpeg -> rose-flower-copy.jpeg로 복사
+    f_src = open("./sample/고루시콘1.jpeg", "rb") # binary 모드로 설정
+    data = f_src.read()
+    print(type(data))
+    f_src.close()
+
+    f_dest = open("./sample/고루시콘1-copy.jpeg", "wb")
+    f_dest.write(data)
+    f_dest.close()
+
+def pickle_dump():
+    with open("./sample/players.bin", "wb") as f: # 자원 자동 정리, pickle 사용을 위해 binary 모드를 이용
+        data = {'baseball': 9}
+        pickle.dump(data, f)
+    print("덤프 완료!")
+
+def pickle_load(): # 객체 역직렬화: 2진 데이터 -> 파이썬 객체로 복원
+    with open("./sample/players.bin", "rb") as f:
+        data = pickle.load(f)
+        print(data, type(data))
+        print("Load 완료!")
+
+def pickle_dump_multi():
+    # dump를 중목 실행하면 여러 객체 dump 가능
+    with open("./sample/players.bin", "wb") as f:
+        pickle.dump({"baseball": 9}, f, 1) # 프로토콜의 버전 명시 가능(ex: 1)
+        pickle.dump({"basketball": 5}, f, pickle.HIGHEST_PROTOCOL) # 가장 최신 버전의 프로토콜
+        pickle.dump({"soccer": 11}, f) # 프로토콜 버전을 명시하지 않으면 가장 최신 버전의 프로토콜 사용
+
+    print("중복 덤프 완료!")
+
+def pickle_load_multi():
+    # 파일 내부에 몇 개의 피클 객체가 저장되어 있는지 확인이 어려움
+    with open("./sample/players.bin", "rb") as f:
+        # print(pickle.load(f))
+        # print(pickle.load(f))
+        # print(pickle.load(f))
+        # print(pickle.load(f)) # End Of File Error 발생 (더 이상 읽을 객체 없음)
+        data_list = []
+        while True:
+            try:
+                data = pickle.load(f)
+            except EOFError:
+                break
+            data_list.append(data)
+
+        print(data_list)
+
+def slamdunk_read():
+    # sangbuk.csv
+    # 한 줄 단위로 읽어서 -> dict -> list에 적재
+    # pickle에 덤프
+    players = []
+    with open("./sample/sangbuk.csv", "rt", encoding="utf-8") as f:
+        while True:
+            line = f.readline()
+            if not line:    # 읽을 데이터가 없다
+                break
+            # 읽은 데이터를 사전화
+            # 정제
+            line = line.strip().replace(" ", "")
+            info = line.split(",")
+            print(info)
+            member = { "name": info[0], "backno": info[1], "height": info[2], "position": info[3]}
+            # print(info)
+            players.append(member)
+
+    print(players)
+    # pickle에 덤프
+    with open("./sample/sangbuk_players.bin", "wb") as f:
+        pickle.dump(players, f)
+
+    print("피클 덤프 완료!")
+
+
+if __name__ == "__main__":
+    # write01()
+    # write02()
+    # read01()
+    # read02()
+    # read03()
+    # copy_binary()
+    # using_pickle()
+    # pickle_dump()
+    # pickle_dump_multi()
+    # pickle_load_multi()
+    slamdunk_read()
